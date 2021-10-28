57033783caef4635f7a835028a62b296e6db85ac
Anuruddhacha <e17038@eng.pdn.ac.lk>
2021-10-29 00:09:49 +0530

    commit 1

---
+++ app/Models/CollectorFarmer.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use App\Models\DailyRecord;
+
+class CollectorFarmer extends Model
+{
+    use HasFactory;
+
+    // public function dailyRecord()
+    // {
+    //     return $this->hasMany(DailyRecord::class);
+    // }//1-m
+
+    protected $fillable = [
+        'status',
+        'collector_id',
+        'farmer_id'
+    ];
+}
